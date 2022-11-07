f1efa982a4da577e095944adf85920e574bebe29
Davis Willard <dwillard@dwillard.dwillard.42.fr>
2022-11-07 23:57:23 +0300

    mandatory done

--- requirements/nginx/tools/entrypoint.sh
+++ requirements/nginx/tools/entrypoint.sh
@@ -6,13 +6,12 @@ openssl req -x509 \
 	-sha256 \
 	-nodes \
 	-days 365 \
-	-subj "/C=RU/ST=Tatarstan/L=Kazan/O=School21/CN=localhost" \
-	-newkey rsa:2048 \
+	-subj "/C=RU/ST=Tatarstan/L=Kazan/O=School21/OU=dwillard/CN=localhost" \
+	-newkey rsa:4096 \
 	-keyout /etc/ssl/private/nginx.key \
 	-out /etc/ssl/certs/nginx.crt;
 
 chown -R www-data:www-data /etc/ssl/;
-chown -R www-data:www-data /etc/ssl;
 chown -R www-data:www-data /var/lib/nginx;
 
 exec "$@"
