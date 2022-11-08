fc283997258192e4ff8eab4bf87d39a508530c42
Davis Willard <dwillard@dwillard.dwillard.42.fr>
2022-11-08 20:29:44 +0300

    installed

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-if [ -f /var/www/wordpress/wp-config.php ]
+if ! wp core version --allow-root --path=/var/www/wordpress &>/dev/null 2>&1
 then
-	sed -ie s/'$DB_NAME'/$DB_NAME/g var/www/wordpress/wp-config.php
-	sed -ie s/'$DB_USER'/$DB_USER/g var/www/wordpress/wp-config.php
-	sed -ie s/'$DB_PASSWORD'/$DB_PASSWORD/g var/www/wordpress/wp-config.php
-	rm -f /var/www/wordpress/wp-config-sample.php
-else
-    echo "Config not found..."
-    exit 1
+	wp core download --allow-root \
+		--path=/var/www/wordpress \
+#		--version=latest \
 fi
+
+if [ ! -f /var/www/wordpress/wp-config.php ]
+then
+	wp config create --allow-root \
+		--path=/var/www/wordpress \
+		--dbname=$DB_NAME \
+		--dbuser=$DB_USER \
+		--dbpass=$DB_PASSWORD \
+		--dbhost=$DB_HOST
+fi
+
+if ! wp core is-installed --allow-root --path=/var/www/wordpress
+then
+	wp core install \
+		--allow-root \
+		--path=/var/www/wordpress \
+		--url=$DOMAIN_NAME \
+		--title=$WP_TITLE \
+		--admin_user=$WP_ADMIN_USER \
+		--admin_password=$WP_ADMIN_PASSWORD \
+		--admin_email=$WP_ADMIN_EMAIL \
+		--skip-email
+fi
+
+#if [ ! -f /var/www/wordpress/wp-config.php ]
+#then
+#    echo "Config not found..."
+#    exit 1
+#fi
 
 mkdir -p ./run/php/
 
