fb57b0dc28324c9b00c540fd5790ec8807b15ea3
Davis Willard <dwillard@dwillard.dwillard.42.fr>
2022-11-09 17:07:38 +0300

    redis kinda done

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -15,6 +15,12 @@ then
 		--dbuser=$DB_USER \
 		--dbpass=$DB_PASSWORD \
 		--dbhost=$DB_HOST
+
+wp config set --allow-root --path=/var/www/wordpress --anchor="/**#@+" --separator="\n\n" WP_REDIS_HOST redis
+wp config set --allow-root --path=/var/www/wordpress --anchor="/**#@+" --separator="\n\n" --raw WP_REDIS_PORT 6379
+wp config set --allow-root --path=/var/www/wordpress --anchor="/**#@+" --separator="\n\n" --raw WP_REDIS_TIMEOUT 1
+wp config set --allow-root --path=/var/www/wordpress --anchor="/**#@+" --separator="\n\n" --raw WP_REDIS_READ_TIMEOUT 1
+wp config set --allow-root --path=/var/www/wordpress --anchor="/**#@+" --separator="\n\n" --raw WP_REDIS_DATABASE 0
 fi
 
 if ! wp core is-installed --allow-root --path=/var/www/wordpress
@@ -30,11 +36,17 @@ then
 		--skip-email
 fi
 
-#if [ ! -f /var/www/wordpress/wp-config.php ]
-#then
-#    echo "Config not found..."
-#    exit 1
-#fi
+if ! wp plugin is-installed --allow-root --path=/var/www/wordpress redis-cache
+then
+	wp plugin install redis-cache \
+		--allow-root \
+		--path=/var/www/wordpress \
+		--activate
+fi
+
+wp redis enable \
+	--allow-root \
+	--path=/var/www/wordpress
 
 mkdir -p ./run/php/
 
